d283e2c16bb66ad529ab7cf29e5bd1e2709d13da
Udimberto Silvestre Junior <udi@ingresse.com>
2019-02-23 23:46:48 -0300

    Updates

---
+++ precache-manifest.d911011a34de41ba93545fc6a50630bd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "43c5c41bce6f5f7d5383e74976ac76c0",
+    "url": "/ingresse-front-end-challenge/static/media/custom.43c5c41b.woff"
+  },
+  {
+    "revision": "3b932277fa4752e4a895",
+    "url": "/ingresse-front-end-challenge/static/css/main.16aaee09.chunk.css"
+  },
+  {
+    "revision": "a46a59f49512955bda5f",
+    "url": "/ingresse-front-end-challenge/static/js/runtime~main.a46a59f4.js"
+  },
+  {
+    "revision": "77b6354ef13b10adb45d4f81c9224ec5",
+    "url": "/ingresse-front-end-challenge/static/media/custom-bold.77b6354e.svg"
+  },
+  {
+    "revision": "bf3c2234145ec04df916",
+    "url": "/ingresse-front-end-challenge/static/js/2.bf3c2234.chunk.js"
+  },
+  {
+    "revision": "cd0b239724ef884b1a4ca94c7c474248",
+    "url": "/ingresse-front-end-challenge/static/media/custom.cd0b2397.woff2"
+  },
+  {
+    "revision": "e665cc9166381708e125991affe2abfd",
+    "url": "/ingresse-front-end-challenge/static/media/custom-bold.e665cc91.woff2"
+  },
+  {
+    "revision": "0b628ce3018820bc552f30025766ac6a",
+    "url": "/ingresse-front-end-challenge/static/media/custom-bold.0b628ce3.woff"
+  },
+  {
+    "revision": "3b932277fa4752e4a895",
+    "url": "/ingresse-front-end-challenge/static/js/main.3b932277.chunk.js"
+  },
+  {
+    "revision": "7eb50ea6ca5d70ff2bfec70912d0fb2e",
+    "url": "/ingresse-front-end-challenge/static/media/custom.7eb50ea6.eot"
+  },
+  {
+    "revision": "a377c50ce3652a13f176bc878280785a",
+    "url": "/ingresse-front-end-challenge/static/media/custom.a377c50c.ttf"
+  },
+  {
+    "revision": "255fb355f36a3b99c5c60e1f413773b1",
+    "url": "/ingresse-front-end-challenge/static/media/custom-bold.255fb355.ttf"
+  },
+  {
+    "revision": "10319942f19766aa82849504beecda84",
+    "url": "/ingresse-front-end-challenge/static/media/custom-bold.10319942.eot"
+  },
+  {
+    "revision": "bb655170ba8dcb647aca9310962da12e",
+    "url": "/ingresse-front-end-challenge/static/media/custom.bb655170.svg"
+  },
+  {
+    "revision": "bf3c2234145ec04df916",
+    "url": "/ingresse-front-end-challenge/static/css/2.8ad0c433.chunk.css"
+  },
+  {
+    "revision": "9e20fa18bcacaa3ec59198dabe8a2ae6",
+    "url": "/ingresse-front-end-challenge/index.html"
+  }
+];
